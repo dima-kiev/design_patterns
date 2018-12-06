7db81ef916ba754c14e9a64ee777d759c5ba2c57
dshk <dima.kiev@gmail.com>
2018-12-06 23:02:39 +0200

    Factory field removed

--- src/com/ridnaxata/designpatterns/creational/abstractfactory/Restaurant.java
+++ src/com/ridnaxata/designpatterns/creational/abstractfactory/Restaurant.java
@@ -5,20 +5,12 @@ public class Restaurant {
     private Dish dish;
     private Drink drink;
 
-    private MenuFactory factory;
-
     public Restaurant(MenuFactory factory) {
-        this.factory = factory;
-    }
-
-    private void obtainMenu() {
         dish = factory.getDish();
         drink = factory.getDrink();
     }
 
     public void showMenu() {
-
-        obtainMenu();
 
         System.out.println("Welcome to our Restaurant! Today in our menu: ");
         System.out.println("main dish: " + dish.getName());
